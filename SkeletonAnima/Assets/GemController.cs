8d7cf234a3dcd9e2aeaa00cb5fc28dadf450b1f1
Vitalii Slobodianyk <slobodyanyck@gmail.com>
2019-09-29 19:39:22 +0300

    some fixes

---
+++ SkeletonAnima/Assets/GemController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+
+}
